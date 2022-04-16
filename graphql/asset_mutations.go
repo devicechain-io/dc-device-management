4baabf3659c1f5d0070a0c5b669d035ec97e4c48
Derek Adams <dadams@figure.com>
2022-04-16 12:01:51 -0400

    refactor asset mutation/api

--- graphql/asset_mutations.go
+++ graphql/asset_mutations.go
@@ -10,40 +10,20 @@ import (
 	"context"
 
 	"github.com/devicechain-io/dc-device-management/model"
-	"github.com/devicechain-io/dc-microservice/rdb"
 )
 
 // Create a new asset type.
 func (r *SchemaResolver) CreateAssetType(ctx context.Context, args struct {
-	Value *model.AssetTypeCreateRequest
+	Request *model.AssetTypeCreateRequest
 }) (*AssetTypeResolver, error) {
-	rdbmgr := r.GetRdbManager(ctx)
-	created := model.AssetType{
-		TokenReference: rdb.TokenReference{
-			Token: args.Value.Token,
-		},
-		NamedEntity: rdb.NamedEntity{
-			Name:        rdb.NullStrOf(args.Value.Name),
-			Description: rdb.NullStrOf(args.Value.Description),
-		},
-		BrandedEntity: rdb.BrandedEntity{
-			ImageUrl:        rdb.NullStrOf(args.Value.ImageUrl),
-			Icon:            rdb.NullStrOf(args.Value.Icon),
-			BackgroundColor: rdb.NullStrOf(args.Value.BackgroundColor),
-			ForegroundColor: rdb.NullStrOf(args.Value.ForegroundColor),
-			BorderColor:     rdb.NullStrOf(args.Value.BorderColor),
-		},
-		MetadataEntity: rdb.MetadataEntity{
-			Metadata: rdb.MetadataStrOf(args.Value.Metadata),
-		},
-	}
-	result := rdbmgr.Database.Create(&created)
-	if result.Error != nil {
-		return nil, result.Error
+	api := r.GetApi(ctx)
+	created, err := api.CreateAssetType(ctx, args.Request)
+	if err != nil {
+		return nil, err
 	}
 
 	dt := &AssetTypeResolver{
-		M: created,
+		M: *created,
 		S: r,
 		C: ctx,
 	}
@@ -52,33 +32,17 @@ func (r *SchemaResolver) CreateAssetType(ctx context.Context, args struct {
 
 // Update an existing asset type.
 func (r *SchemaResolver) UpdateAssetType(ctx context.Context, args struct {
-	Token string
-	Value *model.AssetTypeCreateRequest
+	Token   string
+	Request *model.AssetTypeCreateRequest
 }) (*AssetTypeResolver, error) {
-	found, err := r.AssetTypeByToken(ctx, struct{ Token string }{Token: args.Token})
+	api := r.GetApi(ctx)
+	updated, err := api.UpdateAssetType(ctx, args.Token, args.Request)
 	if err != nil {
 		return nil, err
 	}
 
-	rdbmgr := r.GetRdbManager(ctx)
-	upd := found.M
-	upd.Token = args.Value.Token
-	upd.Name = rdb.NullStrOf(args.Value.Name)
-	upd.Description = rdb.NullStrOf(args.Value.Description)
-	upd.ImageUrl = rdb.NullStrOf(args.Value.ImageUrl)
-	upd.Icon = rdb.NullStrOf(args.Value.Icon)
-	upd.BackgroundColor = rdb.NullStrOf(args.Value.BackgroundColor)
-	upd.ForegroundColor = rdb.NullStrOf(args.Value.ForegroundColor)
-	upd.BorderColor = rdb.NullStrOf(args.Value.BorderColor)
-	upd.Metadata = rdb.MetadataStrOf(args.Value.Metadata)
-
-	result := rdbmgr.Database.Save(&upd)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-
 	dt := &AssetTypeResolver{
-		M: upd,
+		M: *updated,
 		S: r,
 		C: ctx,
 	}
@@ -87,34 +51,16 @@ func (r *SchemaResolver) UpdateAssetType(ctx context.Context, args struct {
 
 // Create a new asset.
 func (r *SchemaResolver) CreateAsset(ctx context.Context, args struct {
-	Value *model.AssetCreateRequest
+	Request *model.AssetCreateRequest
 }) (*AssetResolver, error) {
-	found, err := r.AssetTypeByToken(ctx, struct{ Token string }{Token: args.Value.AssetTypeToken})
+	api := r.GetApi(ctx)
+	created, err := api.CreateAsset(ctx, args.Request)
 	if err != nil {
 		return nil, err
 	}
 
-	rdbmgr := r.GetRdbManager(ctx)
-	created := model.Asset{
-		TokenReference: rdb.TokenReference{
-			Token: args.Value.Token,
-		},
-		NamedEntity: rdb.NamedEntity{
-			Name:        rdb.NullStrOf(args.Value.Name),
-			Description: rdb.NullStrOf(args.Value.Description),
-		},
-		MetadataEntity: rdb.MetadataEntity{
-			Metadata: rdb.MetadataStrOf(args.Value.Metadata),
-		},
-		AssetType: &found.M,
-	}
-	result := rdbmgr.Database.Create(&created)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-
 	dt := &AssetResolver{
-		M: created,
+		M: *created,
 		S: r,
 		C: ctx,
 	}
@@ -123,38 +69,17 @@ func (r *SchemaResolver) CreateAsset(ctx context.Context, args struct {
 
 // Update an existing asset.
 func (r *SchemaResolver) UpdateAsset(ctx context.Context, args struct {
-	Token string
-	Value *model.AssetCreateRequest
+	Token   string
+	Request *model.AssetCreateRequest
 }) (*AssetResolver, error) {
-	found, err := r.AssetByToken(ctx, struct{ Token string }{Token: args.Token})
+	api := r.GetApi(ctx)
+	updated, err := api.UpdateAsset(ctx, args.Token, args.Request)
 	if err != nil {
 		return nil, err
 	}
 
-	// Update fields that changed.
-	rdbmgr := r.GetRdbManager(ctx)
-	upd := found.M
-	upd.Token = args.Value.Token
-	upd.Name = rdb.NullStrOf(args.Value.Name)
-	upd.Description = rdb.NullStrOf(args.Value.Description)
-	upd.Metadata = rdb.MetadataStrOf(args.Value.Metadata)
-
-	// Update asset type if changed.
-	if args.Value.AssetTypeToken != upd.AssetType.Token {
-		found, err := r.AssetTypeByToken(ctx, struct{ Token string }{Token: args.Value.AssetTypeToken})
-		if err != nil {
-			return nil, err
-		}
-		upd.AssetType = &found.M
-	}
-
-	result := rdbmgr.Database.Save(&upd)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-
 	dt := &AssetResolver{
-		M: upd,
+		M: *updated,
 		S: r,
 		C: ctx,
 	}
@@ -163,28 +88,16 @@ func (r *SchemaResolver) UpdateAsset(ctx context.Context, args struct {
 
 // Create a new asset relationship type.
 func (r *SchemaResolver) CreateAssetRelationshipType(ctx context.Context, args struct {
-	Value *model.AssetRelationshipTypeCreateRequest
+	Request *model.AssetRelationshipTypeCreateRequest
 }) (*AssetRelationshipTypeResolver, error) {
-	rdbmgr := r.GetRdbManager(ctx)
-	created := model.AssetRelationshipType{
-		TokenReference: rdb.TokenReference{
-			Token: args.Value.Token,
-		},
-		NamedEntity: rdb.NamedEntity{
-			Name:        rdb.NullStrOf(args.Value.Name),
-			Description: rdb.NullStrOf(args.Value.Description),
-		},
-		MetadataEntity: rdb.MetadataEntity{
-			Metadata: rdb.MetadataStrOf(args.Value.Metadata),
-		},
-	}
-	result := rdbmgr.Database.Create(&created)
-	if result.Error != nil {
-		return nil, result.Error
+	api := r.GetApi(ctx)
+	created, err := api.CreateAssetRelationshipType(ctx, args.Request)
+	if err != nil {
+		return nil, err
 	}
 
 	dt := &AssetRelationshipTypeResolver{
-		M: created,
+		M: *created,
 		S: r,
 		C: ctx,
 	}
@@ -193,28 +106,17 @@ func (r *SchemaResolver) CreateAssetRelationshipType(ctx context.Context, args s
 
 // Update an existing asset relationship type.
 func (r *SchemaResolver) UpdateAssetRelationshipType(ctx context.Context, args struct {
-	Token string
-	Value *model.AssetRelationshipTypeCreateRequest
+	Token   string
+	Request *model.AssetRelationshipTypeCreateRequest
 }) (*AssetRelationshipTypeResolver, error) {
-	found, err := r.AssetRelationshipTypeByToken(ctx, struct{ Token string }{Token: args.Token})
+	api := r.GetApi(ctx)
+	updated, err := api.UpdateAssetRelationshipType(ctx, args.Token, args.Request)
 	if err != nil {
 		return nil, err
 	}
 
-	rdbmgr := r.GetRdbManager(ctx)
-	upd := found.M
-	upd.Token = args.Value.Token
-	upd.Name = rdb.NullStrOf(args.Value.Name)
-	upd.Description = rdb.NullStrOf(args.Value.Description)
-	upd.Metadata = rdb.MetadataStrOf(args.Value.Metadata)
-
-	result := rdbmgr.Database.Save(&upd)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-
 	dt := &AssetRelationshipTypeResolver{
-		M: upd,
+		M: *updated,
 		S: r,
 		C: ctx,
 	}
@@ -223,39 +125,16 @@ func (r *SchemaResolver) UpdateAssetRelationshipType(ctx context.Context, args s
 
 // Create a new asset relationship.
 func (r *SchemaResolver) CreateAssetRelationship(ctx context.Context, args struct {
-	Value *model.AssetRelationshipCreateRequest
+	Request *model.AssetRelationshipCreateRequest
 }) (*AssetRelationshipResolver, error) {
-	rdbmgr := r.GetRdbManager(ctx)
-
-	// Look up token references.
-	source, err := r.AssetByToken(ctx, struct{ Token string }{Token: args.Value.SourceAsset})
+	api := r.GetApi(ctx)
+	created, err := api.CreateAssetRelationship(ctx, args.Request)
 	if err != nil {
 		return nil, err
-	}
-	target, err := r.AssetByToken(ctx, struct{ Token string }{Token: args.Value.TargetAsset})
-	if err != nil {
-		return nil, err
-	}
-	rtype, err := r.AssetRelationshipTypeByToken(ctx, struct{ Token string }{Token: args.Value.RelationshipType})
-	if err != nil {
-		return nil, err
-	}
-
-	created := model.AssetRelationship{
-		SourceAsset:      source.M,
-		TargetAsset:      target.M,
-		RelationshipType: rtype.M,
-		MetadataEntity: rdb.MetadataEntity{
-			Metadata: rdb.MetadataStrOf(args.Value.Metadata),
-		},
-	}
-	result := rdbmgr.Database.Create(&created)
-	if result.Error != nil {
-		return nil, result.Error
 	}
 
 	dt := &AssetRelationshipResolver{
-		M: created,
+		M: *created,
 		S: r,
 		C: ctx,
 	}
@@ -264,70 +143,35 @@ func (r *SchemaResolver) CreateAssetRelationship(ctx context.Context, args struc
 
 // Create a new asset group.
 func (r *SchemaResolver) CreateAssetGroup(ctx context.Context, args struct {
-	Value *model.AssetGroupCreateRequest
+	Request *model.AssetGroupCreateRequest
 }) (*AssetGroupResolver, error) {
-	rdbmgr := r.GetRdbManager(ctx)
-	created := model.AssetGroup{
-		TokenReference: rdb.TokenReference{
-			Token: args.Value.Token,
-		},
-		NamedEntity: rdb.NamedEntity{
-			Name:        rdb.NullStrOf(args.Value.Name),
-			Description: rdb.NullStrOf(args.Value.Description),
-		},
-		BrandedEntity: rdb.BrandedEntity{
-			ImageUrl:        rdb.NullStrOf(args.Value.ImageUrl),
-			Icon:            rdb.NullStrOf(args.Value.Icon),
-			BackgroundColor: rdb.NullStrOf(args.Value.BackgroundColor),
-			ForegroundColor: rdb.NullStrOf(args.Value.ForegroundColor),
-			BorderColor:     rdb.NullStrOf(args.Value.BorderColor),
-		},
-		MetadataEntity: rdb.MetadataEntity{
-			Metadata: rdb.MetadataStrOf(args.Value.Metadata),
-		},
-	}
-	result := rdbmgr.Database.Create(&created)
-	if result.Error != nil {
-		return nil, result.Error
+	api := r.GetApi(ctx)
+	created, err := api.CreateAssetGroup(ctx, args.Request)
+	if err != nil {
+		return nil, err
 	}
 
 	dt := &AssetGroupResolver{
-		M: created,
+		M: *created,
 		S: r,
 		C: ctx,
 	}
 	return dt, nil
 }
 
-// Update an existing asset group.
+// Update an existing asset type.
 func (r *SchemaResolver) UpdateAssetGroup(ctx context.Context, args struct {
-	Token string
-	Value *model.AssetGroupCreateRequest
+	Token   string
+	Request *model.AssetGroupCreateRequest
 }) (*AssetGroupResolver, error) {
-	found, err := r.AssetGroupByToken(ctx, struct{ Token string }{Token: args.Token})
+	api := r.GetApi(ctx)
+	updated, err := api.UpdateAssetGroup(ctx, args.Token, args.Request)
 	if err != nil {
 		return nil, err
 	}
 
-	rdbmgr := r.GetRdbManager(ctx)
-	upd := found.M
-	upd.Token = args.Value.Token
-	upd.Name = rdb.NullStrOf(args.Value.Name)
-	upd.Description = rdb.NullStrOf(args.Value.Description)
-	upd.ImageUrl = rdb.NullStrOf(args.Value.ImageUrl)
-	upd.Icon = rdb.NullStrOf(args.Value.Icon)
-	upd.BackgroundColor = rdb.NullStrOf(args.Value.BackgroundColor)
-	upd.ForegroundColor = rdb.NullStrOf(args.Value.ForegroundColor)
-	upd.BorderColor = rdb.NullStrOf(args.Value.BorderColor)
-	upd.Metadata = rdb.MetadataStrOf(args.Value.Metadata)
-
-	result := rdbmgr.Database.Save(&upd)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-
 	dt := &AssetGroupResolver{
-		M: upd,
+		M: *updated,
 		S: r,
 		C: ctx,
 	}
@@ -336,28 +180,16 @@ func (r *SchemaResolver) UpdateAssetGroup(ctx context.Context, args struct {
 
 // Create a new asset group relationship type.
 func (r *SchemaResolver) CreateAssetGroupRelationshipType(ctx context.Context, args struct {
-	Value *model.AssetGroupRelationshipTypeCreateRequest
+	Request *model.AssetGroupRelationshipTypeCreateRequest
 }) (*AssetGroupRelationshipTypeResolver, error) {
-	rdbmgr := r.GetRdbManager(ctx)
-	created := model.AssetGroupRelationshipType{
-		TokenReference: rdb.TokenReference{
-			Token: args.Value.Token,
-		},
-		NamedEntity: rdb.NamedEntity{
-			Name:        rdb.NullStrOf(args.Value.Name),
-			Description: rdb.NullStrOf(args.Value.Description),
-		},
-		MetadataEntity: rdb.MetadataEntity{
-			Metadata: rdb.MetadataStrOf(args.Value.Metadata),
-		},
-	}
-	result := rdbmgr.Database.Create(&created)
-	if result.Error != nil {
-		return nil, result.Error
+	api := r.GetApi(ctx)
+	created, err := api.CreateAssetGroupRelationshipType(ctx, args.Request)
+	if err != nil {
+		return nil, err
 	}
 
 	dt := &AssetGroupRelationshipTypeResolver{
-		M: created,
+		M: *created,
 		S: r,
 		C: ctx,
 	}
@@ -366,28 +198,17 @@ func (r *SchemaResolver) CreateAssetGroupRelationshipType(ctx context.Context, a
 
 // Update an existing asset group relationship type.
 func (r *SchemaResolver) UpdateAssetGroupRelationshipType(ctx context.Context, args struct {
-	Token string
-	Value *model.AssetGroupRelationshipTypeCreateRequest
+	Token   string
+	Request *model.AssetGroupRelationshipTypeCreateRequest
 }) (*AssetGroupRelationshipTypeResolver, error) {
-	found, err := r.AssetGroupRelationshipTypeByToken(ctx, struct{ Token string }{Token: args.Token})
+	api := r.GetApi(ctx)
+	updated, err := api.UpdateAssetGroupRelationshipType(ctx, args.Token, args.Request)
 	if err != nil {
 		return nil, err
 	}
 
-	rdbmgr := r.GetRdbManager(ctx)
-	upd := found.M
-	upd.Token = args.Value.Token
-	upd.Name = rdb.NullStrOf(args.Value.Name)
-	upd.Description = rdb.NullStrOf(args.Value.Description)
-	upd.Metadata = rdb.MetadataStrOf(args.Value.Metadata)
-
-	result := rdbmgr.Database.Save(&upd)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-
 	dt := &AssetGroupRelationshipTypeResolver{
-		M: upd,
+		M: *updated,
 		S: r,
 		C: ctx,
 	}
@@ -396,39 +217,16 @@ func (r *SchemaResolver) UpdateAssetGroupRelationshipType(ctx context.Context, a
 
 // Create a new asset group relationship.
 func (r *SchemaResolver) CreateAssetGroupRelationship(ctx context.Context, args struct {
-	Value *model.AssetGroupRelationshipCreateRequest
+	Request *model.AssetGroupRelationshipCreateRequest
 }) (*AssetGroupRelationshipResolver, error) {
-	rdbmgr := r.GetRdbManager(ctx)
-
-	// Look up token references.
-	source, err := r.AssetGroupByToken(ctx, struct{ Token string }{Token: args.Value.AssetGroup})
+	api := r.GetApi(ctx)
+	created, err := api.CreateAssetGroupRelationship(ctx, args.Request)
 	if err != nil {
 		return nil, err
-	}
-	target, err := r.AssetByToken(ctx, struct{ Token string }{Token: args.Value.Asset})
-	if err != nil {
-		return nil, err
-	}
-	rtype, err := r.AssetGroupRelationshipTypeByToken(ctx, struct{ Token string }{Token: args.Value.RelationshipType})
-	if err != nil {
-		return nil, err
-	}
-
-	created := model.AssetGroupRelationship{
-		AssetGroup:       source.M,
-		Asset:            target.M,
-		RelationshipType: rtype.M,
-		MetadataEntity: rdb.MetadataEntity{
-			Metadata: rdb.MetadataStrOf(args.Value.Metadata),
-		},
-	}
-	result := rdbmgr.Database.Create(&created)
-	if result.Error != nil {
-		return nil, result.Error
 	}
 
 	dt := &AssetGroupRelationshipResolver{
-		M: created,
+		M: *created,
 		S: r,
 		C: ctx,
 	}
--- model/api_assets.go
+++ model/api_assets.go
@@ -1,5 +1,5 @@
 /**
- * Copyright ©2022 DeviceChain - All Rights Reserved.
+ * Copyright ©2022 AssetChain - All Rights Reserved.
  * Unauthorized copying of this file, via any medium is strictly prohibited.
  * Proprietary and confidential.
  */
@@ -9,8 +9,62 @@ package model
 import (
 	"context"
 
+	"github.com/devicechain-io/dc-microservice/rdb"
 	"gorm.io/gorm"
 )
+
+// Create a new asset type.
+func (api *Api) CreateAssetType(ctx context.Context, request *AssetTypeCreateRequest) (*AssetType, error) {
+	created := &AssetType{
+		TokenReference: rdb.TokenReference{
+			Token: request.Token,
+		},
+		NamedEntity: rdb.NamedEntity{
+			Name:        rdb.NullStrOf(request.Name),
+			Description: rdb.NullStrOf(request.Description),
+		},
+		BrandedEntity: rdb.BrandedEntity{
+			ImageUrl:        rdb.NullStrOf(request.ImageUrl),
+			Icon:            rdb.NullStrOf(request.Icon),
+			BackgroundColor: rdb.NullStrOf(request.BackgroundColor),
+			ForegroundColor: rdb.NullStrOf(request.ForegroundColor),
+			BorderColor:     rdb.NullStrOf(request.BorderColor),
+		},
+		MetadataEntity: rdb.MetadataEntity{
+			Metadata: rdb.MetadataStrOf(request.Metadata),
+		},
+	}
+	result := api.RDB.Database.Create(created)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return created, nil
+}
+
+// Update an existing asset type.
+func (api *Api) UpdateAssetType(ctx context.Context, token string,
+	request *AssetTypeCreateRequest) (*AssetType, error) {
+	found, err := api.AssetTypeByToken(ctx, request.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	found.Token = request.Token
+	found.Name = rdb.NullStrOf(request.Name)
+	found.Description = rdb.NullStrOf(request.Description)
+	found.ImageUrl = rdb.NullStrOf(request.ImageUrl)
+	found.Icon = rdb.NullStrOf(request.Icon)
+	found.BackgroundColor = rdb.NullStrOf(request.BackgroundColor)
+	found.ForegroundColor = rdb.NullStrOf(request.ForegroundColor)
+	found.BorderColor = rdb.NullStrOf(request.BorderColor)
+	found.Metadata = rdb.MetadataStrOf(request.Metadata)
+
+	result := api.RDB.Database.Save(found)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return found, nil
+}
 
 // Get asset type by id.
 func (api *Api) AssetTypeById(ctx context.Context, id uint) (*AssetType, error) {
@@ -48,10 +102,68 @@ func (api *Api) AssetTypes(ctx context.Context, criteria AssetTypeSearchCriteria
 	}, nil
 }
 
+// Create a new asset.
+func (api *Api) CreateAsset(ctx context.Context, request *AssetCreateRequest) (*Asset, error) {
+	dtr, err := api.AssetTypeByToken(ctx, request.AssetTypeToken)
+	if err != nil {
+		return nil, err
+	}
+
+	created := &Asset{
+		TokenReference: rdb.TokenReference{
+			Token: request.Token,
+		},
+		NamedEntity: rdb.NamedEntity{
+			Name:        rdb.NullStrOf(request.Name),
+			Description: rdb.NullStrOf(request.Description),
+		},
+		MetadataEntity: rdb.MetadataEntity{
+			Metadata: rdb.MetadataStrOf(request.Metadata),
+		},
+		AssetType: dtr,
+	}
+	result := api.RDB.Database.Create(created)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return created, nil
+}
+
+// Update an existing asset.
+func (api *Api) UpdateAsset(ctx context.Context, token string, request *AssetCreateRequest) (*Asset, error) {
+	updated, err := api.AssetByToken(ctx, request.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	// Update fields that changed.
+	updated.Token = request.Token
+	updated.Name = rdb.NullStrOf(request.Name)
+	updated.Description = rdb.NullStrOf(request.Description)
+	updated.Metadata = rdb.MetadataStrOf(request.Metadata)
+
+	// Update asset type if changed.
+	if request.AssetTypeToken != updated.AssetType.Token {
+		dtr, err := api.AssetTypeByToken(ctx, request.AssetTypeToken)
+		if err != nil {
+			return nil, err
+		}
+		updated.AssetType = dtr
+	}
+
+	result := api.RDB.Database.Save(updated)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return updated, nil
+}
+
 // Get asset by id.
 func (api *Api) AssetById(ctx context.Context, id uint) (*Asset, error) {
 	found := &Asset{}
-	result := api.RDB.Database.First(found, id)
+	result := api.RDB.Database
+	result = result.Preload("AssetType")
+	result = result.First(found, id)
 	if result.Error != nil {
 		return nil, result.Error
 	}
@@ -61,7 +173,9 @@ func (api *Api) AssetById(ctx context.Context, id uint) (*Asset, error) {
 // Get asset by token.
 func (api *Api) AssetByToken(ctx context.Context, token string) (*Asset, error) {
 	found := &Asset{}
-	result := api.RDB.Database.First(&found, "token = ?", token)
+	result := api.RDB.Database
+	result = result.Preload("AssetType")
+	result = result.First(&found, "token = ?", token)
 	if result.Error != nil {
 		return nil, result.Error
 	}
@@ -88,6 +202,47 @@ func (api *Api) Assets(ctx context.Context, criteria AssetSearchCriteria) (*Asse
 		Results:    results,
 		Pagination: pag,
 	}, nil
+}
+
+// Create a new asset relationship type.
+func (api *Api) CreateAssetRelationshipType(ctx context.Context, request *AssetRelationshipTypeCreateRequest) (*AssetRelationshipType, error) {
+	created := &AssetRelationshipType{
+		TokenReference: rdb.TokenReference{
+			Token: request.Token,
+		},
+		NamedEntity: rdb.NamedEntity{
+			Name:        rdb.NullStrOf(request.Name),
+			Description: rdb.NullStrOf(request.Description),
+		},
+		MetadataEntity: rdb.MetadataEntity{
+			Metadata: rdb.MetadataStrOf(request.Metadata),
+		},
+	}
+	result := api.RDB.Database.Create(created)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return created, nil
+}
+
+// Update an existing asset relationship type.
+func (api *Api) UpdateAssetRelationshipType(ctx context.Context, token string,
+	request *AssetRelationshipTypeCreateRequest) (*AssetRelationshipType, error) {
+	updated, err := api.AssetRelationshipTypeByToken(ctx, request.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	updated.Token = request.Token
+	updated.Name = rdb.NullStrOf(request.Name)
+	updated.Description = rdb.NullStrOf(request.Description)
+	updated.Metadata = rdb.MetadataStrOf(request.Metadata)
+
+	result := api.RDB.Database.Save(updated)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return updated, nil
 }
 
 // Get asset relationship type by id.
@@ -127,6 +282,37 @@ func (api *Api) AssetRelationshipTypes(ctx context.Context,
 	}, nil
 }
 
+// Create a new asset relationship.
+func (api *Api) CreateAssetRelationship(ctx context.Context, request *AssetRelationshipCreateRequest) (*AssetRelationship, error) {
+	// Look up token references.
+	source, err := api.AssetByToken(ctx, request.SourceAsset)
+	if err != nil {
+		return nil, err
+	}
+	target, err := api.AssetByToken(ctx, request.TargetAsset)
+	if err != nil {
+		return nil, err
+	}
+	rtype, err := api.AssetRelationshipTypeByToken(ctx, request.RelationshipType)
+	if err != nil {
+		return nil, err
+	}
+
+	created := &AssetRelationship{
+		SourceAsset:      *source,
+		TargetAsset:      *target,
+		RelationshipType: *rtype,
+		MetadataEntity: rdb.MetadataEntity{
+			Metadata: rdb.MetadataStrOf(request.Metadata),
+		},
+	}
+	result := api.RDB.Database.Create(created)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return created, nil
+}
+
 // Get asset relationship by id.
 func (api *Api) AssetRelationshipById(ctx context.Context, id uint) (*AssetRelationship, error) {
 	found := &AssetRelationship{}
@@ -155,6 +341,59 @@ func (api *Api) AssetRelationships(ctx context.Context,
 		Results:    results,
 		Pagination: pag,
 	}, nil
+}
+
+// Create a new asset group.
+func (api *Api) CreateAssetGroup(ctx context.Context, request *AssetGroupCreateRequest) (*AssetGroup, error) {
+	created := &AssetGroup{
+		TokenReference: rdb.TokenReference{
+			Token: request.Token,
+		},
+		NamedEntity: rdb.NamedEntity{
+			Name:        rdb.NullStrOf(request.Name),
+			Description: rdb.NullStrOf(request.Description),
+		},
+		BrandedEntity: rdb.BrandedEntity{
+			ImageUrl:        rdb.NullStrOf(request.ImageUrl),
+			Icon:            rdb.NullStrOf(request.Icon),
+			BackgroundColor: rdb.NullStrOf(request.BackgroundColor),
+			ForegroundColor: rdb.NullStrOf(request.ForegroundColor),
+			BorderColor:     rdb.NullStrOf(request.BorderColor),
+		},
+		MetadataEntity: rdb.MetadataEntity{
+			Metadata: rdb.MetadataStrOf(request.Metadata),
+		},
+	}
+	result := api.RDB.Database.Create(created)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return created, nil
+}
+
+// Update an existing asset type.
+func (api *Api) UpdateAssetGroup(ctx context.Context, token string,
+	request *AssetGroupCreateRequest) (*AssetGroup, error) {
+	updated, err := api.AssetGroupByToken(ctx, request.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	updated.Token = request.Token
+	updated.Name = rdb.NullStrOf(request.Name)
+	updated.Description = rdb.NullStrOf(request.Description)
+	updated.ImageUrl = rdb.NullStrOf(request.ImageUrl)
+	updated.Icon = rdb.NullStrOf(request.Icon)
+	updated.BackgroundColor = rdb.NullStrOf(request.BackgroundColor)
+	updated.ForegroundColor = rdb.NullStrOf(request.ForegroundColor)
+	updated.BorderColor = rdb.NullStrOf(request.BorderColor)
+	updated.Metadata = rdb.MetadataStrOf(request.Metadata)
+
+	result := api.RDB.Database.Save(updated)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return updated, nil
 }
 
 // Get asset group by id.
@@ -193,6 +432,48 @@ func (api *Api) AssetGroups(ctx context.Context, criteria AssetGroupSearchCriter
 	}, nil
 }
 
+// Create a new asset group relationship type.
+func (api *Api) CreateAssetGroupRelationshipType(ctx context.Context,
+	request *AssetGroupRelationshipTypeCreateRequest) (*AssetGroupRelationshipType, error) {
+	created := &AssetGroupRelationshipType{
+		TokenReference: rdb.TokenReference{
+			Token: request.Token,
+		},
+		NamedEntity: rdb.NamedEntity{
+			Name:        rdb.NullStrOf(request.Name),
+			Description: rdb.NullStrOf(request.Description),
+		},
+		MetadataEntity: rdb.MetadataEntity{
+			Metadata: rdb.MetadataStrOf(request.Metadata),
+		},
+	}
+	result := api.RDB.Database.Create(created)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return created, nil
+}
+
+// Update an existing asset group relationship type.
+func (api *Api) UpdateAssetGroupRelationshipType(ctx context.Context, token string,
+	request *AssetGroupRelationshipTypeCreateRequest) (*AssetGroupRelationshipType, error) {
+	updated, err := api.AssetGroupRelationshipTypeByToken(ctx, request.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	updated.Token = request.Token
+	updated.Name = rdb.NullStrOf(request.Name)
+	updated.Description = rdb.NullStrOf(request.Description)
+	updated.Metadata = rdb.MetadataStrOf(request.Metadata)
+
+	result := api.RDB.Database.Save(updated)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return updated, nil
+}
+
 // Get asset group relationship type by id.
 func (api *Api) AssetGroupRelationshipTypeById(ctx context.Context, id uint) (*AssetGroupRelationshipType, error) {
 	found := &AssetGroupRelationshipType{}
@@ -228,6 +509,39 @@ func (api *Api) AssetGroupRelationshipTypes(ctx context.Context,
 		Results:    results,
 		Pagination: pag,
 	}, nil
+}
+
+// Create a new asset group relationship.
+func (api *Api) CreateAssetGroupRelationship(ctx context.Context,
+	request *AssetGroupRelationshipCreateRequest) (*AssetGroupRelationship, error) {
+
+	// Look up token references.
+	source, err := api.AssetGroupByToken(ctx, request.AssetGroup)
+	if err != nil {
+		return nil, err
+	}
+	target, err := api.AssetByToken(ctx, request.Asset)
+	if err != nil {
+		return nil, err
+	}
+	rtype, err := api.AssetGroupRelationshipTypeByToken(ctx, request.RelationshipType)
+	if err != nil {
+		return nil, err
+	}
+
+	created := &AssetGroupRelationship{
+		AssetGroup:       *source,
+		Asset:            *target,
+		RelationshipType: *rtype,
+		MetadataEntity: rdb.MetadataEntity{
+			Metadata: rdb.MetadataStrOf(request.Metadata),
+		},
+	}
+	result := api.RDB.Database.Create(created)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return created, nil
 }
 
 // Get asset group relationship by id.
